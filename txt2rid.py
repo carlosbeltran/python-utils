f1f88cb30928ac3a923cbc23056f6a2a97699468
Carlos Beltran-Gonzalez <cbeltran@libero.it>
2013-03-27 19:33:33 +0100

    Visualizing images and rect on sequeze

--- txt2rid.py
+++ txt2rid.py
@@ -4,6 +4,9 @@ import os
 import optparse
 import fnmatch
 import shutil
+import sys
+import pygame
+import time
 
 
 def getfileslist(path, extfilter):
@@ -25,28 +28,38 @@ def getbbfromfile(file):
         locations.append(line.strip().split()[:5])
     return locations
 
-parser = optparse.OptionParser()
+if __name__ == '__main__':
 
-parser.add_option("-i", "--imagesdirectory", dest="imagespath",
-        help="directory where to search for images files",metavar="IMGPATH")
+    parser = optparse.OptionParser()
+    
+    parser.add_option("-i", "--imagesdirectory", dest="imagespath",
+            help="directory where to search for images files",metavar="IMGPATH")
+    
+    parser.add_option("-t", "--textsdirectory", dest="txtspath",
+            help ="directory where to search for txt files (Dollar format)", 
+            metavar="TXTPATH")
+    
+    (opts,args) = parser.parse_args()
+    
+    pygame.init()
+    w = 1260
+    h = 960
+    size = (w,h)
+    screen = pygame.display.set_mode(size)
+    c = pygame.time.Clock()
 
-parser.add_option("-t", "--textsdirectory", dest="txtspath",
-        help ="directory where to search for txt files (Dollar format)", 
-        metavar="TXTPATH")
-
-(opts,args) = parser.parse_args()
-
-counter = 0
-
-txtfiles = getfileslist(opts.txtspath, "*.txt")
-imgfiles = getfileslist(opts.imagespath,"*.jpg")
-
-for img,txt in zip(imgfiles,txtfiles):
-    #print opts.imagespath + file 
-    print img + " --> " + txt
-    boundingboxes = getbbfromfile(opts.txtspath+txt)
-    for bb in boundingboxes:
-        print bb
-
-#go back to original dir
+    txtfiles = getfileslist(opts.txtspath, "*.txt")
+    imgfiles = getfileslist(opts.imagespath,"*.jpg")
+    
+    for img,txt in zip(imgfiles,txtfiles):
+        #print opts.imagespath + file 
+        theimg = pygame.image.load(opts.imagespath+img)
+        print img + " --> " + txt
+        boundingboxes = getbbfromfile(opts.txtspath+txt)
+        for bb in boundingboxes:
+            print bb
+            print "----> " + bb[1] + " " + bb[2] + " " + bb[3] + " " + bb[4]
+            pygame.draw.rect(theimg, pygame.Color(255,0,0), pygame.Rect(int(bb[1]),int(bb[2]),int(bb[3]),int(bb[4])),2)
+        screen.blit(theimg,(0,0))
+        pygame.display.flip()
 
