7ccf12d64ba52b303c46fc9c1f9ceb7dfede2261
Carlos Beltran-Gonzalez <cbeltran@libero.it>
2013-03-28 16:00:01 +0100

    Introduced balanced aspect ratio crop

--- txt2rid.py
+++ txt2rid.py
@@ -8,6 +8,7 @@ import sys
 #import pygame
 import time
 from PIL import Image
+import re
 
 
 def getfileslist(path, extfilter):
@@ -28,6 +29,34 @@ def getbbfromfile(file):
     for line in f.readlines()[1:]:
         locations.append(line.strip().split()[:5])
     return locations
+
+def forceaspectratio(box, imgwidth, imghight):
+    x = box[0]
+    y = box[1]
+    width = box[2]
+    height = box[3]
+
+    if width < (height/2):
+        offset = (height/2) - width;
+
+        x = x - (offset/2)
+        width = width + offset
+
+        #not valid box
+        if (x < 0) or ((x+width) > imgwidth):
+            return None
+
+    elif width > (height/2):
+        offset = (width*2) - height
+        y = y - (offset/2)
+        height = height + offset
+
+        if ( y < 0) or ((y+height) > imghight):
+            return None
+    else:
+        print "Perfect box match"
+
+    return (x,y,width,height)
 
 if __name__ == '__main__':
 
@@ -52,17 +81,19 @@ if __name__ == '__main__':
 
     txtfiles = getfileslist(opts.txtspath, "*.txt")
     imgfiles = getfileslist(opts.imagespath,"*.jpg")
+
     
     for img,txt in zip(imgfiles,txtfiles):
         #print opts.imagespath + file 
         imgpath = opts.imagespath+img
         #theimg = pygame.image.load(imgpath)
         pilimg = Image.open(imgpath)
-        print img + " --> " + txt
+        ###print img + " --> " + txt
         boundingboxes = getbbfromfile(opts.txtspath+txt)
         for bb in boundingboxes:
             #check dic
             personid = bb[0]
+            id = int(re.search('\d\d',bb[0]).group(0))
             if personid in people:
                 people[personid] = people[personid] + 1
             else:
@@ -72,11 +103,18 @@ if __name__ == '__main__':
             print "----> " + bb[1] + " " + bb[2] + " " + bb[3] + " " + bb[4]
 
             #pygame.draw.rect(theimg, pygame.Color(255,0,0), pygame.Rect(int(bb[1]),int(bb[2]),int(bb[3]),int(bb[4])),2)
-            box = (int(bb[1]),int(bb[2]), int(bb[1])+int(bb[3]), int(bb[2])+int(bb[4]))
-            area = pilimg.crop(box)
-            newimgname = personid + str(people[personid]) + ".jpg"
-            area.save(opts.imagespath + newimgname,'jpeg')
-        #screen.blit(theimg,(0,0))
-        #pygame.display.flip()
+            box = (int(bb[1]),int(bb[2]), int(bb[3]), int(bb[4]))
+            cropedbox = forceaspectratio(box, 1260, 960)
+            if cropedbox:
+               print "-Ar-> " + str(cropedbox[0]) + " " + str(cropedbox[1]) \
+               + " " + str(cropedbox[2]) + " " + str(cropedbox[3])
+               newbox = (cropedbox[0],
+                       cropedbox[1], 
+                       cropedbox[0]+cropedbox[2],
+                       cropedbox[1]+cropedbox[3])
+
+               #area = pilimg.crop(newbox)
+               #newimgname = "%.4d%.3d.jpg" % (id,people[personid])
+               #area.save(opts.imagespath + "crops/"+ newimgname,'jpeg')
     print people
 
