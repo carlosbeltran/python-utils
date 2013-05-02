82530afa05343f3743edfedaecf3fea7b1d88ceb
Carlos Beltran-Gonzalez <cbeltran@libero.it>
2013-05-02 16:40:45 +0200

    Organizing a little the code.

--- pubclassifier.py
+++ pubclassifier.py
@@ -4,30 +4,18 @@ import sys
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
-#lista = ['aa', 'ab', 'ac']
-f = open("final.txt",'r')
-lista = f.readlines()
-
-topiclist = ['Social Signal Processing',
-        'Video Analytics',
-        'Acoustic Signal Processing',
-        'Sensor and Data Fusion, Mobile Sensing 3D Modeling and Analysis',
-        'Machine Learning Mouse Behavior Analysis In-Vitro Neuronal Network',
-        'Analysis Structural and Functional Brain Connectivity Analysis',
-        'Clustering Analysis for drug discovery',
-        'Other works']
 
 class MyTable(QTableWidget):
-    def __init__(self, *args):
+    def __init__(self, list1,list2,*args):
         QTableWidget.__init__(self, *args)
-        self.buildtablestruct()
+        self.buildtablestruct(list1,list2)
         self.verticalHeader().setDefaultSectionSize(100)
         self.horizontalHeader().setDefaultSectionSize(500)
         self.resize(1000,500*2)
 
-    def buildtablestruct(self):
-        for i in range(len(lista)):
-            newitem = QTableWidgetItem(lista[i])
+    def buildtablestruct(self, listapubs,topiclist):
+        for i in range(len(listapubs)):
+            newitem = QTableWidgetItem(listapubs[i])
     
             combo = QComboBox()
             for topic in topiclist:
@@ -37,8 +25,19 @@ class MyTable(QTableWidget):
             self.setCellWidget(i,1,combo)
 
 def main(args):
+    f = open("final.txt",'r')
+    lista = f.readlines()
+    
+    topiclist = ['Social Signal Processing',
+            'Video Analytics',
+            'Acoustic Signal Processing',
+            'Sensor and Data Fusion, Mobile Sensing 3D Modeling and Analysis',
+            'Machine Learning Mouse Behavior Analysis In-Vitro Neuronal Network',
+            'Analysis Structural and Functional Brain Connectivity Analysis',
+            'Clustering Analysis for drug discovery',
+            'Other works']
     app = QApplication(args)
-    table = MyTable(len(lista), 2)
+    table = MyTable(lista,topiclist,len(lista), 2)
     table.show()
     sys.exit(app.exec_())
     
