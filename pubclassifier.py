1f7d232c310b00405caf2ce6395d28df524d4f6c
Carlos Beltran-Gonzalez <cbeltran@libero.it>
2013-05-02 14:32:12 +0200

    Just added a list to manage the topics.

--- pubclassifier.py
+++ pubclassifier.py
@@ -7,6 +7,16 @@ from PyQt4.QtGui import *
 lista = ['aa', 'ab', 'ac']
 listb = ['ba', 'bb', 'bc']
 listc = ['ca', 'cb', 'cc']
+
+topiclist = ['Social Signal Processing',
+        'Video Analytics',
+        'Acoustic Signal Processing',
+        'Sensor and Data Fusion, Mobile Sensing 3D Modeling and Analysis',
+        'Machine Learning Mouse Behavior Analysis In-Vitro Neuronal Network',
+        'Analysis Structural and Functional Brain Connectivity Analysis',
+        'Clustering Analysis for drug discovery',
+        'Other works']
+
 mystruct = {'A':lista, 'B':listb, 'C':listc}
 
 class MyTable(QTableWidget):
@@ -14,23 +24,9 @@ class MyTable(QTableWidget):
         QTableWidget.__init__(self, *args)
         self.data = thestruct
         self.combo = QComboBox()
-        self.combo.addItem("Social Signal Processing")
-        self.combo.addItem("Video Analytics")
-        self.combo.addItem("Acoustic Signal Processing")
-        self.combo.addItem("Sensor and Data Fusion, Mobile Sensing 3D Modeling and Analysis")
-        self.combo.addItem("Machine Learning Mouse Behavior Analysis In-Vitro Neuronal Network")
-        self.combo.addItem("Analysis Structural and Functional Brain Connectivity Analysis")
-        self.combo.addItem("Clustering Analysis for drug discovery")
-        self.combo.addItem("Other works")
 
-        #Social Signal Processing
-        #Video Analytics
-        #Acoustic Signal Processing
-        #Sensor and Data Fusion, Mobile Sensing 3D Modeling and Analysis
-        #Machine Learning Mouse Behavior Analysis In-Vitro Neuronal Network
-        #Analysis Structural and Functional Brain Connectivity Analysis
-        #Clustering Analysis for drug discovery
-        #Other works
+        for topic in topiclist:
+            self.combo.addItem(topic)
 
         self.setmydata()
         
